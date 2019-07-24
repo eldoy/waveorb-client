b46244ac86a11b2fa6d52fbabd498711e73154a2
Vidar Eldøy <vidar@eldoy.com>
2019-07-24 18:10:17 +0200

    Update demo

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -16,6 +16,18 @@
 		console.log({ result })
 	}
 
+	async function handleActions() {
+		console.log('Running actions')
+		let result = await ajax('tasks/insert')({ name: 'test' })
+		console.log({ result })
+		result = await ajax('tasks/update')({ _id: result._id }, { name: 'test2' })
+		console.log({ result })
+		result = await ajax('tasks/update')({ name: 'test2' }, { name: 'test3' })
+		console.log({ result })
+		result = await ajax('tasks/get')({ name: 'test3' })
+		console.log({ result })
+	}
+
 	async function run() {
 		// AJAX
 		console.log('AJAX')
@@ -33,6 +45,8 @@
 			message: async function(data, event) {
 				console.log('SUBSCRIPTION MESSAGE RECEIVED')
 				console.log({ data })
+				const result = await ajax('uploads/get')({ _id: data[0] })
+				console.log(result)
 			}
 		})
 		console.log({ result })
@@ -49,3 +63,4 @@
 
 <h1>Waveorb client demo!</h1>
 <button on:click="{handleUpload}">Upload</button>
+<button on:click="{handleActions}">Actions</button>
