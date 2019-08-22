4fd06cfe715f96b4944602dda83678e759ce5419
Vidar Eldøy <vidar@eldoy.com>
2019-08-22 02:03:54 +0200

    Remove logs

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Socket = require('wsrecon')
 const axios = require('axios')
-const isBrowser = typeof window !== 'undefined'
-console.log({ isBrowser })
 const SOCKET_OPTIONS = { reconnect: 1000, ping: 3000 }
 const DEFAULT_CONFIG = { host: 'localhost:4000', ssl: false, ws: true }
 
@@ -33,32 +31,26 @@ module.exports = function(customConfig = {}) {
   if (config.ws) {
     socket = new Socket(url('ws'), SOCKET_OPTIONS)
     socket.on('open', async (event) => {
-      console.log('Connection open')
       if (typeof events.open === 'function') {
         await events.open(event)
       }
     })
 
     socket.on('close', async (event) => {
-      console.log('Connection closed')
       if (typeof events.close === 'function') {
         await events.close(event)
       }
     })
 
     socket.on('error', async (event) => {
-      console.log('Connection error')
       if (typeof events.error === 'function') {
         await events.error(event)
       }
     })
 
     socket.on('message', async (data, event) => {
-      console.log('Received message', data)
       const sub = data.result.sub
       if (sub) {
-        console.log('SUB DATA')
-        console.log(sub)
         await subs[sub.path](sub, event)
       } else if (typeof events.message === 'function') {
         await events.message(data.result, event)
@@ -70,19 +62,16 @@ module.exports = function(customConfig = {}) {
     return async function (...data) {
       const params = tokenize({ db: { path, data } })
       const run = await socket.fetch(params)
-      console.log(JSON.stringify(run.result))
       return run.result
     }
   }
 
   ws.on = function(event, fn) {
-    console.log('REGISTERING EVENT:', event)
     events[event] = fn
   }
 
   async function send(params, config = {}) {
     const post = (await axios.post(url('http'), params, config)).data
-    console.log(JSON.stringify(post.result))
     return post.result
   }
 
@@ -123,7 +112,6 @@ module.exports = function(customConfig = {}) {
           input.accept = options.accept
         }
         async function change() {
-          console.log('CHANGE')
           const files = input.files
           const params = formdata(path, options, files)
 
@@ -150,7 +138,6 @@ module.exports = function(customConfig = {}) {
       }
       const params = { subs: paths }
       const run = await socket.fetch(params)
-      console.log(JSON.stringify(run.result))
       return run.result
     }
   }
