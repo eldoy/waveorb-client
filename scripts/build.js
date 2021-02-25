d61e3debc32099cc638aa523ad250813cb983daa
Vidar Eldøy <vidar@eldoy.com>
2021-02-25 05:25:10 +0100

    Update packages

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const uglify = require('uglify-es')
+const pretty = require('pretty')
 const fspath = require('path')
 const client = require('../index.js')
 const http = require(fspath.join(process.cwd(), 'node_modules', 'taarn', 'browser.js'))
@@ -8,16 +9,16 @@ const socket = require(fspath.join(process.cwd(), 'node_modules', 'wsrecon', 'in
 
 const name = 'waveorb'
 
-const code = `(function() {
+const code = pretty(`(function() {
   var socket = ${socket};
   var http = ${http};
   window.${name} = ${client}
-}())`
+}())`)
 
 if (!fs.existsSync('./dist')) {
   fs.mkdirSync('./dist')
 }
 
-fs.writeFileSync('./dist/waveorb.js', code)
+fs.writeFileSync(`./dist/${name}.js`, code)
 const result = uglify.minify(code)
 fs.writeFileSync(`./dist/${name}-min.js`, result.code)
